8046ef0e7bf476af161fa2e9016ee3b82b90c0e1
Yukkuritaku <taku565758@gmail.com>
2025-04-26 15:17:16 +0900

    Remove unused imports

--- src/main/java/com/github/yukkuritaku/modernwarpmenu/client/gui/components/ScaleTransitionButton.java
+++ src/main/java/com/github/yukkuritaku/modernwarpmenu/client/gui/components/ScaleTransitionButton.java
@@ -13,7 +13,6 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ARGB;
-import net.minecraft.util.Mth;
 import org.joml.Matrix4f;
 
 import java.awt.*;
